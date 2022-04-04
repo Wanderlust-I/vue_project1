9a37cd961ea5358723891424b5f258d57fc671bc
wanderlust <2231812981@qq.com>
2022-04-04 20:31:00 +0800

    分类功能完成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
 import axios  from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
+Vue.component('tree-table', TreeTable)
 axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config=>{
   console.log(config);
